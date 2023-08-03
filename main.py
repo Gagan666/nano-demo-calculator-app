a860a9222fa5033e30d834c5baefaf1d04b87658
Gagan <gaganyadav2002@gmail.com>
2023-08-04 01:08:38 +0530

    adding demo code modified 3

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/calculator/greeting', methods=['GET'])
+@app.route('/caluclator/greeting', methods=['GET'])
 def greeting():
     return "Hello world!", 200
 
