0e7194296be45877830598ddcff370d313191860
Gagan <gaganyadav2002@gmail.com>
2023-08-06 23:45:45 +0530

    adding demo code modified 11

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 import requests
 from flask import Flask, request, jsonify
 
@@ -26,4 +25,5 @@ def sub():
     return jsonify({'result':res})
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(port=8080,host='0.0.0.0')
