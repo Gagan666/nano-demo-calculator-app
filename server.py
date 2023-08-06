a0609e14e88f61535325d786a49dcd21f18ec5c0
Gagan <gaganyadav2002@gmail.com>
2023-08-06 23:56:59 +0530

    adding demo code modified 13

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return "Hello World!"
+    return "Hello World!", 200
 
 @app.route('/calculator/add',methods=['POST'])
 def add():
@@ -14,7 +14,7 @@ def add():
     first = data.get('first',0)
     second = data.get('second',0)
     res = first+second
-    return jsonify({'result':res})
+    return jsonify({'result':res}),200
 
 @app.route('/calculator/subtract',methods=['POST'])
 def sub():
@@ -22,8 +22,7 @@ def sub():
     first = data.get('first',0)
     second = data.get('second',0)
     res = first-second
-    return jsonify({'result':res})
+    return jsonify({'result':res}),200
 
 
-if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+app.run()
