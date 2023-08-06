cab713c31c4ea713b6b5abfe8b31ff05b7d517d3
Gagan <gaganyadav2002@gmail.com>
2023-08-07 00:14:16 +0530

    lol

--- server.py
+++ server.py
@@ -1,29 +1,31 @@
 import requests
 from flask import Flask, request, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 
 @app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return "Hello World!", 200
+    return "Hello World!"
 
-@app.route('/calculator/add',methods=['POST'])
+
+@app.route('/calculator/add', methods=['POST'])
 def add():
     data = request.get_json()
-    first = data.get('first',0)
-    second = data.get('second',0)
+    first = data.get('first', 0)
+    second = data.get('second', 0)
     res = first+second
-    return jsonify({'result':res}),200
+    return jsonify({'result': res})
 
-@app.route('/calculator/subtract',methods=['POST'])
+
+@app.route('/calculator/subtract', methods=['POST'])
 def sub():
     data = request.get_json()
-    first = data.get('first',0)
-    second = data.get('second',0)
+    first = data.get('first', 0)
+    second = data.get('second', 0)
     res = first-second
-    return jsonify({'result':res}),200
+    return jsonify({'result': res})
 
 
-if _name_ == '_main_':
-    app.run(port=8080,host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')
