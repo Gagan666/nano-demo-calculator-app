f65970c2574bd1dff2cc4d061f45e2a25cf1527e
Gagan <gaganyadav2002@gmail.com>
2023-08-06 23:27:03 +0530

    adding demo code modified 7

--- server.py
+++ server.py
@@ -1,29 +1,29 @@
+import requests
 from flask import Flask, request, jsonify
-from dataclasses import dataclass
-
-@dataclass
-class Result:
-    result: int
-
 
 app = Flask(__name__)
 
 
-@app.route("/calculator/greeting", methods=['GET'])
+@app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return 'Hello world!'
+    return "Hello World!", 200
 
-@app.route("/calculator/add", methods=['POST'])
+@app.route('/calculator/add',methods=['POST'])
 def add():
-    numbers = request.json
-    response = Result(numbers['first'] + numbers['second'])
-    return jsonify(response)
+    data = request.get_json()
+    first = data.get('first',0)
+    second = data.get('second',0)
+    res = first+second
+    return jsonify({'result':res}),200
 
-@app.route("/calculator/subtract", methods=['POST'])
-def subtract():
-    numbers = request.json
-    response = Result(numbers['first'] - numbers['second'])
-    return jsonify(response)
+@app.route('/calculator/subtract',methods=['POST'])
+def sub():
+    data = request.get_json()
+    first = data.get('first',0)
+    second = data.get('second',0)
+    res = first-second
+    return jsonify({'result':res}),200
+
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
